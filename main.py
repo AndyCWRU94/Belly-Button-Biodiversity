6bedd807cff6f57f1fa8eef90235d8aa51cf0b21
AndyCWRU94 <42708127+AndyCWRU94@users.noreply.github.com>
2019-02-13 18:43:23 -0500

    tweaks

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-#################################################
-# Dependencies
-#################################################
-# Flask (Server)
+
 from flask import Flask, jsonify, render_template, request, flash, redirect
 
 # SQL Alchemy (ORM)
@@ -14,38 +11,28 @@ import pandas as pd
 import numpy as np
 
 
-#################################################
-# Database Setup
-#################################################
+# Db
 engine = create_engine("sqlite:///DataSets/belly_button_biodiversity.sqlite")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
+
 OTU = Base.classes.otu
 Samples = Base.classes.samples
 Samples_Metadata= Base.classes.samples_metadata
 
-# Create our session (link) from Python to the DB
+# Create session
 session = Session(engine)
 
-#################################################
-# Flask Setup
-#################################################
+
 app = Flask(__name__)
-#################################################
-# Flask Routes
-#################################################
-# Returns the dashboard homepage
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-#################################################
-# Returns a list of sample names
+
 @app.route('/names')
 def names():
     """Return a list of sample names."""
@@ -58,7 +45,6 @@ def names():
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns))
 
-#################################################
 # Returns a list of OTU descriptions 
 @app.route('/otu')
 def otu():
@@ -69,7 +55,6 @@ def otu():
     otu_list = list(np.ravel(results))
     return jsonify(otu_list)
 
-#################################################
 # Returns a json dictionary of sample metadata 
 @app.route('/metadata/<sample>')
 def sample_metadata(sample):
@@ -77,13 +62,9 @@ def sample_metadata(sample):
     sel = [Samples_Metadata.SAMPLEID, Samples_Metadata.ETHNICITY,
            Samples_Metadata.GENDER, Samples_Metadata.AGE,
            Samples_Metadata.LOCATION, Samples_Metadata.BBTYPE]
-
-    # sample[3:] strips the `BB_` prefix from the sample name to match
-    # the numeric value of `SAMPLEID` from the database
     results = session.query(*sel).\
         filter(Samples_Metadata.SAMPLEID == sample[3:]).all()
 
-    # Create a dictionary entry for each row of metadata information
     sample_metadata = {}
     for result in results:
         sample_metadata['SAMPLEID'] = result[0]
@@ -95,7 +76,6 @@ def sample_metadata(sample):
 
     return jsonify(sample_metadata)
 
-#################################################
 # Returns an integer value for the weekly washing frequency `WFREQ`
 @app.route('/wfreq/<sample>')
 def sample_wfreq(sample):
@@ -109,7 +89,6 @@ def sample_wfreq(sample):
     # Return only the first integer value for washing frequency
     return jsonify(int(wfreq[0]))
 
-#################################################
 # Return a list of dictionaries containing sorted lists  for `otu_ids`and `sample_values`
 @app.route('/samples/<sample>')
 def samples(sample):
